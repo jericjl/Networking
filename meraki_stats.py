0f2368e1b3b205804b86a6970087efbf66ea1444
jeric232 <lambitjeric@gmail.com>
2021-04-22 18:50:19 +0800

    Script to display all Cisco meraki equipment

---
+++ meraki_stats.py
@@ -0,0 +1,27 @@
+import requests
+import  json
+from credentials import api_key
+
+API_KEY = api_key() # Call the API Key from credentials Module
+#SECRET_KEY = '*****'
+BASE_URL = 'https://api.meraki.com/api/v1/'
+
+#Request parameters
+url_command = 'organizations/{}/devices/statuses'
+
+#BASE_URL
+request_url = BASE_URL + url_command
+
+header = {
+    'X-Cisco-Meraki-API-Key' : API_KEY,
+}
+r = requests.get(request_url,headers=header)
+
+results = r.json()
+#print(json.dumps(result, indent=4))
+
+for data in results:   
+    # print(data)
+    for k,v in data.items():
+        if v == '': #Check if equal to value pair
+            print(json.dumps(data,indent=2))
