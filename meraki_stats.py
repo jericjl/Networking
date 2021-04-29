45e5c7ba4d5ef8c43379c40d6db065ae14ec8dd4
jeric232 <lambitjeric@gmail.com>
2021-04-29 23:36:46 +0800

    modify code and make it more readable

--- meraki_stats.py
+++ meraki_stats.py
@@ -5,17 +5,17 @@ from credentials import org_id
 
 
 API_KEY = api_key() # Call the API Key from credentials Module
-#SECRET_KEY = '*****'
-BASE_URL = 'https://api.meraki.com/api/v1/'
 
-#Request parameters
-url_command = f'organizations/{org_id()}/devices/statuses'
-
-#BASE_URL
-request_url = BASE_URL + url_command
 
 def get_equipment(status):
     equipmentstatus = str(status)
+
+    BASE_URL = 'https://api.meraki.com/api/v1/'
+
+    #Request parameters
+    url_command = f'organizations/{org_id()}/devices/statuses'
+    request_url = BASE_URL + url_command
+
     header = {
         'X-Cisco-Meraki-API-Key' : API_KEY,
     }
@@ -23,14 +23,14 @@ def get_equipment(status):
 
     results = r.json()
     #print(json.dumps(result, indent=4))
-    i = []
+    equipment_list = []
 
     for data in results:   # Extract data from the list
         # print(data)
         for k,v in data.items(): #Extract data from Dictionary
             if v == equipmentstatus: #Check if value is equal to key pair
                 result =  i.append(data) 
-    return i
+    return equipment_list
 
 status = input("Please input : ")
 
